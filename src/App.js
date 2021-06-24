1a36c31d4e2a67cf7d5cfd9410df3ef6b532cc83
Saied Alouardani <salouardani@DEC-B-003.fritz.box>
2021-06-24 09:51:23 +0200

    First changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day
+          Learn React in 240 minutes!!! Yes you can!!!
         </a>
       </header>
     </div>
